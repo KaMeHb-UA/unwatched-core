6407bb34800b6540ba4931c9673a6f427415ee36
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-08-23 14:37:17 +0300

    Preparing to upgrade server

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-//*
 createTemplate('unwatchedProfile', 'utf8', {
     text : "I'll be the unwatched.ml core file!",
 }, function(err, tpl){
     if (!err){
-        console.log(tpl);
         exit(tpl.text);
     }
 });
-//*/
-/*
-var tpl = new Template('unwatchedProfile', 'utf8', {
-    text : "I'll be the unwatched.ml core file!",
-});
-console.log(tpl);
-exit(tpl.text);
-//*/
