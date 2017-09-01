a9b69a4020aae32776c88ec94934b49e6e9f6393
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-09-01 23:24:40 +0300

    Debug update for non-win systems

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var spawn = require('child_process').spawn;
     
-if (/^win/.test(process.platform)) spawn('cmd', ['/s', '/c', 'node nodemon | cmdcolor'],{stdio:'inherit'}); else {
+if (/^win/.test(process.platform) && process.argv.indexOf('--unix') == -1) spawn('cmd', ['/s', '/c', 'node server --unix | cmdcolor'],{stdio:'inherit'}); else {
     /*
     spawn('node', ['nodemon'],{stdio:'inherit'});
     /*/
-    require('fs').chmodSync('./cmdcolor', 0o744);    
-    spawn('bash', ['-c','"node nodemon | ./cmdcolor"'],{stdio:'inherit'});
+    var nodemon = require('nodemon'),
+    dateTime = require('node-datetime'),
+    projectName = (function(a){a=a.split('/');a=a[a.length-1];a=a.split('\\');return a[a.length-1];})(process.cwd());
+    nodemon({ script: 'server-main.js' }).on('restart', function(fn){
+        console.log('\\033[36m' + dateTime.create().format('[d-m-y H:M:S]') + '\\033[33m ' + projectName + ' server restarted due to:\\033[0m ' + fn);
+    });
     //*/
 }
