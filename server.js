bdf03907735f51d7639501a44e8f51fcea4846db
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-09-01 20:19:43 +0300

    Test update for linux-based systems

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var spawn = require('child_process').spawn;
     
-if (/^win/.test(process.platform)) spawn('cmd', ['/s', '/c', 'node nodemon | cmdcolor'],{stdio:'inherit'}); else spawn('node', ['nodemon'],{stdio:'inherit'});
+if (/^win/.test(process.platform)) spawn('cmd', ['/s', '/c', 'node nodemon | cmdcolor'],{stdio:'inherit'}); else {
+    /*
+    spawn('node', ['nodemon'],{stdio:'inherit'});
+    /*/
+    spawn('exec', ['"node nodemon >> ./cmdcolor 2>&1"'],{stdio:'inherit'});
+    //*/
+}
