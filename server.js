086afbb5b0af76e78f2d6626d146b861c9ae444c
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-09-01 17:59:40 +0300

    Next test update

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var spawn = require('child_process').spawn;
     
 if (/^win/.test(process.platform)) spawn('cmd', ['/s', '/c', 'node nodemon | cmdcolor'],{stdio:'inherit'}); else {
-    spawn('node', ['nodemon']).on('data', function(data){
-        process.stdout.write('stdout: ' + data);
-    });
+    spawn('node', ['nodemon']).stdout.pipe(process.stdout);
 } ;
